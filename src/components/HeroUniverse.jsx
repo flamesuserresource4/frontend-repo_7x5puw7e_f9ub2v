541c4d79edca99de5ab2b9521ed169927fbbe857
Flames <vibe-coder@flames.com>
2025-11-07 04:34:30 +0000

    Update project prj_7x5puw7e - 2025-11-07T04:34:30.015Z

--- src/components/HeroUniverse.jsx
+++ src/components/HeroUniverse.jsx
@@ -70,7 +70,7 @@ export default function HeroUniverse() {
       {/* 3D Spline Background */}
       <div className="absolute inset-0">
         <Spline
-          scene="https://prod.spline.design/0CT1-dbOQTa-XJKt/scene.splinecode"
+          scene="https://prod.spline.design/EF7JOSsHLk16Tlw9/scene.splinecode"
           style={{ width: '100%', height: '100%' }}
         />
       </div>
